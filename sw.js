9db217e92e47f8c3f4dddb8dc1d64e67766ff4ba
Sagar Swami Rao <sagarswamirao@gmail.com>
2024-09-11 16:31:00 -0600

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-221005e57ed0277b720f.js"
+    "url": "webpack-runtime-0dfc49233a4e9cb930f6.js"
   },
   {
     "url": "framework-ffb4b47555000dbff0b4.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "64b49c000ae33168751631bd5b84977c"
+    "revision": "2fe0e7681f1676b0f5006aca0d61aa33"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-18294acab2877398415c.js"
